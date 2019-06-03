f44d4667596e8a3587af9eff41bee6a2f3367791
daisuke <dsaito85a@gmail.com>
2019-06-03 16:20:20 +0900

    空の講座管理画面の作成

--- laraReserve/resources/views/layouts/app.blade.php
+++ laraReserve/resources/views/layouts/app.blade.php
@@ -60,6 +60,9 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
+                                    <a class="dropdown-item" href="./course">
+                                        講座管理
+                                    </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
