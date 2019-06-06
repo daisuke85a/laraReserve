1779e1fce2e833c1a62e9093cc38a4a67551e8f4
daisuke <dsaito85a@gmail.com>
2019-06-06 12:58:07 +0900

    一般向けレッスンリストの表示を整えた

--- laraReserve/resources/views/welcome.blade.php
+++ laraReserve/resources/views/welcome.blade.php
@@ -2,78 +2,48 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    @if (count($errors) > 0)
+    <div>
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{$error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    @if (session('status'))
+    <div class="alert alert-success" role="alert">
+        {{ session('status') }}
+    </div>
+    @endif
+
+    <div class="row">
+        @foreach ($courses as $course)
+        <div class="col-md-6">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header"><h2>{{$course->title}}</h2></div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                </div>
-            </div>
-
-            @if (count($errors) > 0)
-            <div>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                    <li>{{$error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-            @endif
-
-            <h2>クラス</h2>
-            <table>
-                <tr>
-                    <th>クラス名</th>
-                    <th>内容</th>
-                    <th>料金</th>
-                    <th>レッスン予定</th>
-                </tr>
-                @foreach ($courses as $course)
-                <tr>
-                    <td>{{$course->title}}</td>
-                    <td>{{$course->content}}</td>
-                    <td>{{$course->fee}}円</td>
-                    <td>
+                    <p>{{$course->content}}</p>
+                    <p>{{$course->fee}}円</p>
+                    <h3>レッスンの予約</h3>
+                    <div class="row">
                         @if (count($course->lessons) > 0)
                         @foreach ($course->lessons as $lesson)
+                        <div class="col-md-3">
                             <p><a href="/{{$course->id}}/lesson/{{$lesson->id}}">{{$lesson->getDate()}}〜</a></p>
+                        </div>
                         @endforeach
                         @else
-                            <p>予定無し</p>
+                        <div class="col-md-3">
+                        <p>レッスン予定無し</p>
+                        </div>
                         @endif
-                    </td>
-
-                    <td>
-                        <form action="/{{$course->id}}/lesson/add" method="GET">
-                            {{ csrf_field() }}
-                            <input type="submit" value="日程追加" class="btn btn-primary btn-sm btn-dell">
-                        </form>
-                    </td>
-
-                    <td>
-                        <form action="/course/edit/{{$course->id}}" method="GET">
-                            {{ csrf_field() }}
-                            <input type="submit" value="編集" class="btn btn-secondary btn-sm btn-dell">
-                        </form>
-                    </td>
-
-                    <td>
-                        <form action="/course/delete/{{$course->id}}" method="POST">
-                            {{ csrf_field() }}
-                            <input type="submit" value="削除" class="btn btn-danger btn-sm btn-dell">
-                        </form>
-                    </td>
-                </tr>
-                @endforeach
-            </table>
-
+                    </div>
+                </div>
+            </div>
         </div>
+        @endforeach
+
     </div>
 </div>
 @endsection
