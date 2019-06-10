143e1be395ce745ec9286d25d948b9f28d407b35
daisuke <dsaito85a@gmail.com>
2019-06-10 10:33:08 +0900

    googlemapを表示する

--- laraReserve/routes/web.php
+++ laraReserve/routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/map', function () {
+     return view('map');
+});
 
 Route::get('/', 'CourseController@welcome');
 
