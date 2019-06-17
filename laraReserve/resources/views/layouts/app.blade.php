2786fa6e3014df407b79635eb16efc908fb35924
daisuke saito <dsaito85a@gmail.com>
2019-06-17 19:58:39 +0900

    ナビからのログインをTwitterログインに変更

--- laraReserve/resources/views/layouts/app.blade.php
+++ laraReserve/resources/views/layouts/app.blade.php
@@ -45,7 +45,7 @@
                         <!-- Authentication Links -->
                         @guest
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            <a class="nav-link" href="{{ url('/login/twitter') }}">{{ __('Login') }}</a>
                         </li>
                         @if (Route::has('register'))
                         <li class="nav-item">
