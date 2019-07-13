e014880830dc9f96b699fde1a2b01f948f9bbf06
daisuke saito <dsaito85a@gmail.com>
2019-07-13 10:54:38 +0900

    全入力に対し文字コードがUTF-8であることを確認する

---
+++ laraReserve/app/Http/Middleware/EncodingValidateParams.php
@@ -0,0 +1,23 @@
+<?php namespace App\Http\Middleware;
+
+use Closure;
+
+class EncodingValidateParams{
+    public function handle($request, Closure $next){
+        foreach($request->all() as $val){
+            if(!$this->isValidEncoding($val)){
+                abort(400,'Bad Request');
+            }
+        }
+
+        return $next($request);
+    }
+
+    private function isValidEncoding($val){
+        if(mb_check_encoding($val,"UTF-8")){
+            return true;
+        }
+
+        return false;
+    }
+}
