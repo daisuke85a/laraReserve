038bf78ef703a60e520fa8189d79112b1fe2904e
daisuke saito <dsaito85a@gmail.com>
2019-07-16 17:05:56 +0900

    テストケースの追加（ただし失敗する）

---
+++ laraReserve/tests/Feature/LoginTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->providerName = 'twitter';
+    }
+
+        /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+
+    /**
+     * @test
+     */
+    public function Twitterの認証画面を表示できる()
+    {
+
+        // $response = $this->get('login/' . $this->providerName);
+        $response = $this->get('login/twitter');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function Twitterアカウントでユーザー登録できる()
+    {
+        $response = $this->get('login/' . $this->providerName .'/callback');
+        $response->assertStatus(200);
+    }
+
+}
