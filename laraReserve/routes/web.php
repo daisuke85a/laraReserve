98f3afa5d96be4842cba27afd56d4882b386dd2a
daisuke saito <dsaito85a@gmail.com>
2019-07-25 17:58:29 +0900

    production環境からBasic認証を削除

--- laraReserve/routes/web.php
+++ laraReserve/routes/web.php
@@ -11,7 +11,7 @@
 |
  */
 
-Route::group(['middleware' => 'auth.very_basic'], function () {
+if (\App::environment('production')) {
     Route::get('/login/{provider}', 'Auth\LoginController@redirectToProvider');
     Route::get('login/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
     Route::post('logout', 'Auth\LoginController@logout')->name('logout');
@@ -50,4 +50,45 @@ Route::group(['middleware' => 'auth.very_basic'], function () {
         return view('terms');
     });
 
-});
+} else {
+    Route::group(['middleware' => 'auth.very_basic'], function () {
+        Route::get('/login/{provider}', 'Auth\LoginController@redirectToProvider');
+        Route::get('login/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
+        Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+        Route::get('/', 'CourseController@welcome');
+        Route::get('/course', 'CourseController@index');
+        Route::get('/course/add', 'CourseController@add');
+        Route::post('/course/create', 'CourseController@create')->middleware('auth');
+
+        Route::get('/course/{id}/', 'CourseController@view'); //参照
+        Route::get('/course/edit/{id}/', 'CourseController@edit_index'); //編集
+        Route::post('/course/edit/{id}/', 'CourseController@edit_finish'); //完了
+        Route::post('/course/delete/{id}/', 'CourseController@delete'); //削除
+
+        Route::get('/{id}/lesson/add/', 'LessonController@add'); //レッスン追加画面表示
+        Route::get('/{course_id}/lesson/{id}', 'LessonController@index'); //レッスン表示
+        Route::post('/{id}/lesson/create/', 'LessonController@create'); //レッスン追加
+        Route::delete('/{course_id}/lesson/{id}/delete', 'LessonController@delete'); //レッスン削除
+
+        Route::get('/reserve/add/{id}', 'ReserveController@add'); //レッスン予約
+        Route::post('/reserve/create/', 'ReserveController@create'); //レッスン予約
+        Route::post('/reserve/delete/', 'ReserveController@delete'); //削除
+
+        Route::post('/like/create/', 'LikeController@create'); //イイね
+        Route::post('/like/delete/', 'LikeController@delete'); //イイね
+
+        Route::get('/user/{userId}', 'UserController@show');
+        Route::get('/user/{userId}/edit', 'UserController@edit');
+        Route::patch('/user/update/{userId}', 'UserController@update'); /*編集フォーム */
+
+        Route::get('/privacy', function () {
+            return view('privacy');
+        });
+
+        Route::get('/terms', function () {
+            return view('terms');
+        });
+
+    });
+}
